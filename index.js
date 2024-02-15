e7e30a7690d198866e64d7ab1d30cd6b18df1698
kingrexicon <oyedeleoreofe@gmail.com>
2024-02-15 03:31:01 +0100

    finished,  guess

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const re =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
 const email = document.getElementById("email");
 const invalid = document.getElementById("invalid");
+var link = document.getElementById("");
 
 function clickBtn() {
   if (re.test(email.value)) {
     location.href = "thanks.html";
-  console.log(String(email.value))
+    console.log(String(email.value));
   } else {
-   invalid.innerText = 'invalid email'
-//    invalid.innerText = ''
+    invalid.innerText = "invalid email";
+    //    invalid.innerText = ''
   }
 }
+
+link.innerText = `${email.value}`;
